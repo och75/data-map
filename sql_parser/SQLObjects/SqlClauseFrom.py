f85cfc465450bbdbce68858c6f8a992aa146d93d
o.charles <olivier.charles@blablacar.com>
2019-04-09 15:32:49 +0200

    dmise en place filtrage des clauses de jointures dans ClauseFrom.iterate()

--- sql_parser/SQLObjects/SqlClauseFrom.py
+++ sql_parser/SQLObjects/SqlClauseFrom.py
@@ -16,6 +16,13 @@ class SqlClauseFrom():
         stop, one_table = SqlTable.iterate_one_table_strings(enum)
         sql_tables.append(one_table)
 
+        index, word=enum.actual()
+
+        #on ne parse pas les clauses de jointures  (mais on pourrait creer un objet JoinClause )
+        while ( word=='on') or (word not in SqlTable.TABLE_DELIMITERS and not stop):
+            index, word = enum.next()
+            stop = word in SqlTable.TABLE_END_ENUM and not stop
+
         if not stop:
             stop, sql_tables = SqlClauseFrom.iterate_from_clause(enum,sql_tables)
 
--- sql_parser/SQLObjects/SqlTable.py
+++ sql_parser/SQLObjects/SqlTable.py
@@ -18,13 +18,6 @@ class SqlTable():
         if one_table is None:
             one_table=SqlTable()
 
-        # si on rencontre "on" il faut avancer le curseur jusqu'qu prochain mot cle TABLE_DELIMITERS
-        #TODO a deplacer dans interate from clause
-        if word=='on':
-            index, word = enum.next()
-            while word not in SqlTable.TABLE_DELIMITERS and word not in SqlTable.TABLE_END_ENUM:
-                index, word = enum.next()
-
         stop_iterate_tables = False if word not in SqlTable.TABLE_END_ENUM else True
 
         if word not in SqlTable.TABLE_DELIMITERS and not stop_iterate_tables:
