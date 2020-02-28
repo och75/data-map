2b0b4194a2626527972768a307d2bca387eeb951
o.charles <olivier.charles@blablacar.com>
2020-02-28 15:27:09 +0100

    upgrade flatatthon

--- very_sql_parser/stackdriver.sql
+++ very_sql_parser/stackdriver.sql
@@ -1,5 +1,69 @@
-create table temp.explore_queries as
+create or replace table temp.explore_queries_4 as
+select
+    t.*,
+    case
+        when type_operation='update' then regexp_extract(query, 'update ([a-z._0-9]*) ')
+        when type_operation='insert' then regexp_extract(query, 'insert into ([a-z._0-9]*) ')
+        when type_operation='drop' then regexp_extract(replace(query, 'if exists ',''), 'drop table ([a-z._0-9]*)')
+        when type_operation='delete' then regexp_extract(replace(query, 'from ',''), 'delete ([a-z._0-9]*) ')
+        when type_operation='create' then regexp_extract(replace(query, 'or replace ',''), 'create table ([a-z._0-9]*) ')
+        else null
+    end
+    as concerned_tables
+from
+  `temp.explore_queries_3`  t;
+
+
+
+  create or replace table temp.explore_queries_3 as
+select
+  key,
+  Date,
+  ProjectId,
+  UserId,
+  Query,
+    REGEXP_EXTRACT(Query, "\\*[\\s\\S]*\\Wtype[\\s]*=[\\s]*'([^']*)'[\\s\\S]*\\*/") as TagType,
+    REGEXP_EXTRACT(Query, "\\*[\\s\\S]*\\Wcategory[\\s]*=[\\s]*'([^']*)'[\\s\\S]*\\*/") as TagCategory,
+    REGEXP_EXTRACT(Query, "\\*[\\s\\S]*\\Wsub_category[\\s]*=[\\s]*'([^']*)'[\\s\\S]*\\*/") as TagSubCategory,
+
+  case
+    when Query like '%select %' then 'select'
+    when Query like '%drop %' then 'drop'
+    when Query like '%create %' then 'create'
+    when Query like '%delete %' then 'delete'
+    when Query like '%update %' then 'update'
+    when Query like '%insert %' then 'insert'
+  end as type_operation,
+  REGEXP_EXTRACT_ALL(Query, '[A-Za-z0-9_]+' ) as ls_words
+from
+  temp.explore_queries_2
+where
+  UserId like 'airflow%';
+
+
+create or replace table temp.explore_queries_2 as
+select
+    t.* except (Query),
+lower(
+    replace(
+      replace(
+        replace(
+          REPLACE(Query,'’', ''),
+          '\n', ' '
+       ),
+       '`',''
+     ),
+    '(',' ('
+   )
+) as Query
+from
+    temp.explore_queries t;
+
+
+
+create or replace table temp.explore_queries as
 SELECT
+    row_number() over() as key,
     timestamp AS Date,
     resource.labels.project_id AS ProjectId,
     protopayload_auditlog.serviceName AS ServiceName,
@@ -27,7 +91,8 @@ SELECT
     protopayload_auditlog.servicedata_v1_bigquery.jobCompletedEvent.job.jobStatistics.totalBilledBytes / pow(2,30) AS TotalBilledGigabytes,
     (protopayload_auditlog.servicedata_v1_bigquery.jobCompletedEvent.job.jobStatistics.totalBilledBytes / pow(2,30)) / pow(2,10) AS TotalBilledTerabytes,
     ((protopayload_auditlog.servicedata_v1_bigquery.jobCompletedEvent.job.jobStatistics.totalBilledBytes / pow(2,30)) / pow(2,10)) * 5 AS TotalCost,
-    lower(REGEXP_EXTRACT(REPLACE(protopayload_auditlog.servicedata_v1_bigquery.jobCompletedEvent.job.jobConfiguration.query.query,'’', '\''), "\\*[\\s\\S]*\\Wtype[\\s]*=[\\s]*'([^']*)'[\\s\\S]*\\*/")) as TagType,
-    lower(REGEXP_EXTRACT(REPLACE(protopayload_auditlog.servicedata_v1_bigquery.jobCompletedEvent.job.jobConfiguration.query.query,'’', '\''), "\\*[\\s\\S]*\\Wcategory[\\s]*=[\\s]*'([^']*)'[\\s\\S]*\\*/")) as TagCategory,
-    lower(REGEXP_EXTRACT(REPLACE(protopayload_auditlog.servicedata_v1_bigquery.jobCompletedEvent.job.jobConfiguration.query.query,'’', '\''), "\\*[\\s\\S]*\\Wsub_category[\\s]*=[\\s]*'([^']*)'[\\s\\S]*\\*/")) as TagSubCategory
-FROM `BigQuery_stackdriver.cloudaudit_googleapis_com_data_access_201911*`
+FROM
+  `BigQuery_stackdriver.cloudaudit_googleapis_com_data_access_20200227*`
+WHERE
+  protopayload_auditlog.servicedata_v1_bigquery.jobCompletedEvent.job.jobConfiguration.query.query is not null
+;
